0299daa849b1e7c667be0559a60ba3fc70596b22
longlevan9x <longvan1296@gmail.com>
2023-05-18 23:18:30 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['https://vuejs-notion-blog.vercel.app'] });
+  app.enableCors({ origin: [process.env.DOMAIN_ALLOW] });
   await app.listen(3000);
 }
 
