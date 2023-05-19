36539b4993181a7ab65ab2c17ec64b7945c2de96
longlevan9x <longvan1296@gmail.com>
2023-05-19 09:39:38 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: [process.env.DOMAIN_ALLOW] });
+
+  const whitelist = process.env.ALLOW_LIST || '';
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(3000);
 }
 
